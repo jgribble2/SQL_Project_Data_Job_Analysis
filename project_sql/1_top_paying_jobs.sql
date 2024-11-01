1d0356922eb8b047933a71ce521ade85e3155ced
jgribble2 <163337288+jgribble2@users.noreply.github.com>
2024-11-01 00:23:19 -0400

    Update 1_top_paying_jobs.sql

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,6 +1,6 @@
 /*
-QUESTION: What are the highest paying data analyst and buisness analyst jobs?
-- find the top 25 highest paying remote data analyst jobs
+QUESTION: What are the highest paying data analyst or buisness analyst jobs?
+- find the top 10 highest paying data or business analyst jobs in Philly
 - remove nulls
 - find company names for each job posting
 - WHY? (highlight top paying data analyst jobs)
@@ -20,8 +20,8 @@ LEFT JOIN
 ON job_postings_fact.company_id = company_dim.company_id
 WHERE
     job_title_short IN ('Data Analyst', 'Business Analyst')
-    AND job_work_from_home = TRUE
+    AND job_location = 'Philadelphia, PA'
     AND salary_year_avg IS NOT NULL
 ORDER BY
     salary_year_avg DESC
-LIMIT 25;
+LIMIT 10;
