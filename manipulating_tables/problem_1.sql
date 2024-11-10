0df6120ab715bbe4ccc836625f41f90ce01c0992
Jack Gribble <jgribble2@outlook.com>
2024-11-10 14:20:02 -0500

    Problem 11

---
+++ manipulating_tables/problem_1.sql
@@ -0,0 +1,9 @@
+-- create a table named "data_science jobs"
+-- include the following columns: "job_id" (integer and primary key), "job_title" (text), "company_name" (text), and "post_date" (date)
+
+CREATE TABLE data_science_jobs (
+    job_id INT,
+    job_title TEXT,
+    company_name TEXT,
+    posted_date DATE
+);
