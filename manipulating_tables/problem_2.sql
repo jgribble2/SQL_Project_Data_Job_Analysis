9b6cef6f2275e7f794bda153633c16f3f8ce6fbe
Jack Gribble <jgribble2@outlook.com>
2024-11-15 18:50:29 -0500

    query 2

---
+++ manipulating_tables/problem_2.sql
@@ -0,0 +1,5 @@
+INSERT INTO data_science_jobs (job_id, job_title, company_name, posted_date)
+VALUES
+    (1, 'Data Scientist', 'Tech Innovations', '2023-01-01'),
+    (2, 'Machine Learning Engineer', 'Data Driven Co', '2023-01-15'),
+    (3, 'AI Specialist', 'Future Tech', '2023-02-01');
